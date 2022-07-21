141c9a72ebff46ee8c8c6708bfaa580b8bcbbf77
Kobe Bantes <102146326+kkoobe11@users.noreply.github.com>
2022-07-21 17:40:13 +0800

    late

--- src/pages/components/nav-bar/left-bar.js
+++ src/pages/components/nav-bar/left-bar.js
@@ -3,26 +3,26 @@ import Link from "next/link";
 
 const Leftbar = () => {
   return (
-    <div className="flex items-center gap-5 pl-4">
-      <div className="flex">
+    <div className="flex items-center justify-center gap-3 pl-1 lg:pl-4">
+      <div className="flex pr-4">
         <h1 className="text-3xl font-semibold lg:text-3xl text-kobe">K</h1>
         <h1 className="text-3xl font-semibold text-white lg:text-3xl">B</h1>
       </div>
 
       <Link href="/about">
-        <div className="relative flex items-center justify-center h-12 mx-auto mt-2 mb-2 font-normal text-white rounded-lg cursor-pointer w-28 hover:bg-gray-700">
+        <div className="relative flex items-center justify-center w-20 h-12 font-normal text-white rounded-lg cursor-pointer lg:w-28 hover:bg-gray-700">
           About Me
         </div>
       </Link>
 
       <Link href="/projects">
-        <div className="relative flex items-center justify-center h-12 mx-auto mt-2 mb-2 font-normal text-white rounded-lg cursor-pointer w-28 hover:bg-gray-700">
+        <div className="relative flex items-center justify-center w-20 h-12 font-normal text-white rounded-lg cursor-pointer lg:w-28 hover:bg-gray-700">
           Projects
         </div>
       </Link>
 
       <Link href="/contact">
-        <div className="relative flex items-center justify-center h-12 mx-auto mt-2 mb-2 font-normal text-white rounded-lg cursor-pointer w-28 hover:bg-gray-700">
+        <div className="relative flex items-center justify-center w-20 h-12 font-normal text-white rounded-lg cursor-pointer lg:w-28 hover:bg-gray-700">
           Contact
         </div>
       </Link>
--- src/pages/components/nav-bar/nav-bar.js
+++ src/pages/components/nav-bar/nav-bar.js
@@ -5,7 +5,7 @@ import Leftbar from "./left-bar";
 const Navbar = () => {
   return (
     <div className="sticky top-0 flex items-center h-16 shadow bg-topblack ">
-      <div className="items-center justify-start w-screen mx-3 lg:flex">
+      <div className="flex items-center justify-center w-screen mx-3 lg:justify-start lg:flex">
         <Leftbar />
       </div>
     </div>
