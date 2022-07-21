4aa9865486a426f63cc4de0d05c8cfb373fc58f9
Kobe Bantes <102146326+kkoobe11@users.noreply.github.com>
2022-07-21 17:50:01 +0800

    changed

--- src/pages/components/center-content/index.js
+++ src/pages/components/center-content/index.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Content = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen pb-40 text-white">
-      <div className="flex items-center justify-center rounded-lg opacity-75 w-96 h-96 bg-zinc-700 lg:w-1/3">
+      <div className="flex items-center justify-center w-9/12 rounded-lg opacity-75 h-96 bg-zinc-700 lg:w-1/3">
       </div>
       <div className="absolute items-center justify-center text-center opacity-100">
         <Image
@@ -18,10 +18,10 @@ const Content = () => {
           alt=""
         />
         <div className="flex items-center justify-center pt-2">
-        <h1 className="text-4xl font-semibold lg:text-5xl text-kobe">K</h1>
-        <h1 className="text-4xl font-semibold lg:text-5xl">obe Priam Bantes</h1>
+        <h1 className="text-3xl font-semibold lg:text-5xl text-kobe">K</h1>
+        <h1 className="text-3xl font-semibold lg:text-5xl">obe Priam Bantes</h1>
         </div>
-        <h1 className="text-xl font-normal lg:text-xl">Junior Software Engineer</h1>
+        <h1 className="text-xl font-normal lg:text-2xl">Junior Software Engineer</h1>
         </div>
     </div>
   );
