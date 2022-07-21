1174dfc0ea176ff70fc67fe642256271ab6e78be
Kobe Bantes <102146326+kkoobe11@users.noreply.github.com>
2022-07-21 23:27:33 +0800

    new

--- src/pages/components/nav-bar/footer.js
+++ src/pages/components/nav-bar/footer.js
@@ -5,7 +5,7 @@ import Centerbar from "./center-bar";
 const Footer = () => {
   return (
     <div className="fixed bottom-0 flex items-center w-full shadow bg-topblack">
-      <div className="items-center justify-center w-full pt-4 mx-3 pb-14 lg:flex md:pb-0 sm:pt-2">
+      <div className="items-center justify-center w-full pt-4 pb-4 mx-3 lg:flex md:pb-0 sm:pt-2">
         <Centerbar />
       </div>
     </div>
