d19c04495245049f77ec3c84e01716384797e0ef
Kobe Bantes <102146326+kkoobe11@users.noreply.github.com>
2022-07-22 23:05:21 +0800

    last

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,33 @@ import Content from "./components/center-content";
 import Footer from "./components/nav-bar/footer";
 import Landing from "./landing/Landing";
 
-const MyComponent = (props) => {
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const myRef = useRef(null)
+export default function Home() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
   useEffect(() => {
     if (!vantaEffect) {
-      setVantaEffect(BIRDS({
-        el: myRef.current,
-        THREE: THREE,
-      }))
+      setVantaEffect(
+        BIRDS({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x3fa4ff,
+        })
+      );
     }
     return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
   return (
-    <div style={{ height: "100vh" }} ref={myRef}>
+    <div style={{ height: "100vh" }} ref={vantaRef}>
       <div className="h-screen overflow-hidden">
         <Navbar />
         <Content />
@@ -31,5 +41,3 @@ const MyComponent = (props) => {
     </div>
   );
 }
-
-export default MyComponent;
